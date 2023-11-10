0171d4303e48e64745d7e02f40ef5b480c77c580
Stephan Kulla <git.mail@kulla.me>
2023-11-10 14:32:01 +0100

    Format download_documents.ts

--- download_documents.ts
+++ download_documents.ts
@@ -1,76 +1,76 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as crypto from "crypto";
-import { exec } from "child_process";
-import { promisify } from "util";
+import * as fs from 'fs'
+import * as path from 'path'
+import * as crypto from 'crypto'
+import { exec } from 'child_process'
+import { promisify } from 'util'
 
-const repoUrl = "https://github.com/your-username/your-repo.git"; // Replace with your repo URL
-const tempDir = path.join(__dirname, "temp");
-const targetDir = "path/to/directory"; // Relative path in the repo
-const documentsDir = path.join(__dirname, "documents");
-const gqlRegex = /gql`([\s\S]*?)`/g;
+const repoUrl = 'https://github.com/your-username/your-repo.git' // Replace with your repo URL
+const tempDir = path.join(__dirname, 'temp')
+const targetDir = 'path/to/directory' // Relative path in the repo
+const documentsDir = path.join(__dirname, 'documents')
+const gqlRegex = /gql`([\s\S]*?)`/g
 
-const execAsync = promisify(exec);
+const execAsync = promisify(exec)
 
 async function cloneRepo() {
   if (!fs.existsSync(tempDir)) {
-    fs.mkdirSync(tempDir, { recursive: true });
+    fs.mkdirSync(tempDir, { recursive: true })
   }
 
-  await execAsync(`git clone ${repoUrl} ${tempDir}`);
+  await execAsync(`git clone ${repoUrl} ${tempDir}`)
 }
 
 function findTSFiles(dir: string): string[] {
-  let filesToProcess: string[] = [];
-  const files = fs.readdirSync(dir);
+  let filesToProcess: string[] = []
+  const files = fs.readdirSync(dir)
 
   for (const file of files) {
-    const fullPath = path.join(dir, file);
-    const stat = fs.statSync(fullPath);
+    const fullPath = path.join(dir, file)
+    const stat = fs.statSync(fullPath)
 
     if (stat.isDirectory()) {
-      filesToProcess = filesToProcess.concat(findTSFiles(fullPath));
-    } else if (file.endsWith(".ts") || file.endsWith(".tsx")) {
-      filesToProcess.push(fullPath);
+      filesToProcess = filesToProcess.concat(findTSFiles(fullPath))
+    } else if (file.endsWith('.ts') || file.endsWith('.tsx')) {
+      filesToProcess.push(fullPath)
     }
   }
 
-  return filesToProcess;
+  return filesToProcess
 }
 
 async function processFiles() {
-  const fullPath = path.join(tempDir, targetDir);
-  const tsFiles = findTSFiles(fullPath);
+  const fullPath = path.join(tempDir, targetDir)
+  const tsFiles = findTSFiles(fullPath)
 
   for (const file of tsFiles) {
-    const fileContent = fs.readFileSync(file, "utf-8");
-    let match;
+    const fileContent = fs.readFileSync(file, 'utf-8')
+    let match
     while ((match = gqlRegex.exec(fileContent)) !== null) {
-      const gqlContent = match[1];
-      const hash = crypto.createHash("md5").update(gqlContent).digest("hex");
-      const graphqlFilePath = path.join(documentsDir, `${hash}.graphql`);
+      const gqlContent = match[1]
+      const hash = crypto.createHash('md5').update(gqlContent).digest('hex')
+      const graphqlFilePath = path.join(documentsDir, `${hash}.graphql`)
 
-      fs.writeFileSync(graphqlFilePath, gqlContent);
+      fs.writeFileSync(graphqlFilePath, gqlContent)
     }
   }
 }
 
 async function prepareDocumentsDir() {
   if (!fs.existsSync(documentsDir)) {
-    fs.mkdirSync(documentsDir);
+    fs.mkdirSync(documentsDir)
   } else {
     fs.readdirSync(documentsDir).forEach((file) => {
-      fs.unlinkSync(path.join(documentsDir, file));
-    });
+      fs.unlinkSync(path.join(documentsDir, file))
+    })
   }
 }
 
 async function main() {
-  await prepareDocumentsDir();
-  await cloneRepo();
-  await processFiles();
+  await prepareDocumentsDir()
+  await cloneRepo()
+  await processFiles()
 
-  console.log("GraphQL queries have been extracted and saved.");
+  console.log('GraphQL queries have been extracted and saved.')
 }
 
-main().catch(console.error);
+main().catch(console.error)
